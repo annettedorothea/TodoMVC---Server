995b78728fd7060693060d093f48100a792bd09e
Annette Pohl <annette_pohl@web.de>
2019-02-11 19:55:03 +0100

    new generator

--- src/gen/java/com/anfelisa/todo/models/ToggleAllModel.java
+++ src/gen/java/com/anfelisa/todo/models/ToggleAllModel.java
@@ -1,3 +1,22 @@
+/* 
+ * Copyright (c) 2019, Annette Pohl, Koblenz, Germany
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+
+
+
 package com.anfelisa.todo.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -55,7 +74,7 @@ public class ToggleAllModel implements IToggleAllModel {
 	}
 	
 	
-	public List<String> equalsPrimitiveTypes(IToggleAllModel other) {
+	public List<String> equalsPrimitiveTypes(com.anfelisa.todo.models.IToggleAllModel other) {
 		List<String> differingAttributes = new ArrayList<String>();
 		if (!(this.getDone() == null && other.getDone() == null) && !this.getDone().equals(other.getDone())) {
 			differingAttributes.add("done: " + this.getDone() + " " + other.getDone());
@@ -68,4 +87,10 @@ public class ToggleAllModel implements IToggleAllModel {
 	
 }
 
-/*       S.D.G.       */
+
+
+
+/******* S.D.G. *******/
+
+
+
--- src/main/java/com/anfelisa/ace/App.java
+++ src/main/java/com/anfelisa/ace/App.java
@@ -1,5 +1,11 @@
 package com.anfelisa.ace;
 
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 import org.jdbi.v3.core.Jdbi;
 import org.slf4j.Logger;
@@ -34,7 +40,7 @@ public class App extends Application<CustomAppConfiguration> {
 	}
 
 	public static String getVersion() {
-		return "0.4.1";
+		return "0.5.0";
 	}
 
 	@Override
@@ -89,6 +95,7 @@ public class App extends Application<CustomAppConfiguration> {
 		AppRegistration.registerResources(environment, jdbi, configuration, daoProvider, viewProvider, e2e);
 		AppRegistration.registerConsumers(viewProvider, mode);
 
+		configureCors(environment);
 	}
 
 	public static void reportException(Exception x) {
@@ -100,5 +107,21 @@ public class App extends Application<CustomAppConfiguration> {
 			}
 		}
 	}
+	
+	private void configureCors(Environment environment) {
+		final FilterRegistration.Dynamic cors = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+		// Configure CORS parameters
+		cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+		cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM,
+				"X-Requested-With,Content-Type,Accept,Origin,Authorization");
+		cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+		cors.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, "true");
+
+		// Add URL mapping
+		cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
+	}
+
 
 }
