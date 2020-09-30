68a11011fc5371c79fd3688924489078711425e6
Annette Pohl <annette_pohl@web.de>
2020-09-30 19:16:26 +0200

    removed CORS stuff

--- src/main/java/de/acegen/App.java
+++ src/main/java/de/acegen/App.java
@@ -1,11 +1,5 @@
 package de.acegen;
 
-import java.util.EnumSet;
-
-import javax.servlet.DispatcherType;
-import javax.servlet.FilterRegistration;
-
-import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 import org.jdbi.v3.core.Jdbi;
 import org.slf4j.Logger;
@@ -83,23 +77,6 @@ public class App extends Application<CustomAppConfiguration> {
 		AppRegistration.registerResources(environment, new PersistenceConnection(jdbi), configuration, daoProvider,
 				viewProvider);
 		AppRegistration.registerConsumers(viewProvider);
-
-		configureCors(environment);
-	}
-
-	private void configureCors(Environment environment) {
-		final FilterRegistration.Dynamic cors = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
-
-		// Configure CORS parameters
-		cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
-		cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM,
-				"X-Requested-With,Content-Type,Accept,Origin,Authorization");
-		cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD");
-		cors.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, "true");
-
-		// Add URL mapping
-		cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
-
 	}
 
 }
