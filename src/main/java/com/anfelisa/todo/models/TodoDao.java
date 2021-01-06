8ced2393dec56e9a04f4b27f8fd8c11b4daab4a2
Annette Pohl <annette_pohl@web.de>
2021-01-06 12:34:26 +0100

    jdbi experiment

--- src/main/java/com/anfelisa/todo/models/TodoDao.java
+++ src/main/java/com/anfelisa/todo/models/TodoDao.java
@@ -1,9 +1,9 @@
 package com.anfelisa.todo.models;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
-
-import org.jdbi.v3.core.statement.Update;
 
 import com.anfelisa.todo.data.IToggleAllData;
 
@@ -12,51 +12,47 @@ import de.acegen.PersistenceHandle;
 public class TodoDao extends AbstractTodoDao {
 	
 	public void toggleTodo(PersistenceHandle handle, ITodoToggleModel model) {
-		Update statement = handle.getHandle()
-				.createUpdate("UPDATE public.todo SET done = :done, updateddatetime = :updateddatetime WHERE id = :id");
-		statement.bind("id", model.getId());
-		statement.bind("done", model.getDone());
-		statement.bind("updateddatetime", model.getUpdatedDateTime());
-		statement.execute();
+		Map<String, Object> params = new HashMap<String, Object> ();
+		params.put("id", model.getId());
+		params.put("done", model.getDone());
+		params.put("updateddatetime", model.getUpdatedDateTime());
+		update(handle, "UPDATE public.todo SET done = :done, updateddatetime = :updateddatetime WHERE id = :id", params);
 	}
 
 	public void updateById(PersistenceHandle handle, ITodoModel todoModel) {
-		Update statement = handle.getHandle().createUpdate(
-				"UPDATE public.todo SET description = :description, updateddatetime = :updateddatetime WHERE id = :id");
-		statement.bind("id", todoModel.getId());
-		statement.bind("description", todoModel.getDescription());
-		statement.bind("updateddatetime", todoModel.getUpdatedDateTime());
-		statement.execute();
+		Map<String, Object> params = new HashMap<String, Object> ();
+		params.put("id", todoModel.getId());
+		params.put("description", todoModel.getDescription());
+		params.put("updateddatetime", todoModel.getUpdatedDateTime());
+		update(handle, "UPDATE public.todo SET description = :description, updateddatetime = :updateddatetime WHERE id = :id", params);
 	}
 
 	public List<ITodoModel> selectAllOrderedByCreatedDate(PersistenceHandle handle, String categoryId) {
-		return handle.getHandle()
-				.createQuery("SELECT * FROM public.todo where categoryid = :categoryid order by createddatetime")
-				.bind("categoryid", categoryId).map(new TodoMapper()).list();
+		Map<String, Object> params = new HashMap<String, Object> ();
+		params.put("categoryid", categoryId);
+		return selectList(handle, "SELECT * FROM public.todo where categoryid = :categoryid order by createddatetime", params, new TodoMapper());
 	}
 
 	public void deleteDone(PersistenceHandle handle, String categoryId) {
-		Update statement = handle.getHandle().createUpdate("DELETE FROM public.todo WHERE done = true and categoryid = :categoryid")
-				.bind("categoryid", categoryId);
-		statement.execute();
+		Map<String, Object> params = new HashMap<String, Object> ();
+		params.put("categoryid", categoryId);
+		update(handle, "DELETE FROM public.todo WHERE done = true and categoryid = :categoryid", params);
 	}
 
 	public void toggleAll(PersistenceHandle handle, IToggleAllData dataContainer) {
 		List<ITodoModel> todos = dataContainer.getTodosToBeToggled();
 		List<String> idList = todos.stream().map(ITodoModel::getId).collect(Collectors.toList());
-		Update statement = handle.getHandle().createUpdate(
-				"UPDATE public.todo SET done = :done, updateddatetime = :updateddatetime where id in (<idlist>)");
-		statement.bind("done", dataContainer.getDone());
-		statement.bindList("idlist", idList);
-		statement.bind("updateddatetime", dataContainer.getUpdatedDateTime());
-		statement.execute();
+		Map<String, Object> params = new HashMap<String, Object> ();
+		params.put("done", dataContainer.getDone());
+		params.put("idlist", idList);
+		params.put("updateddatetime", dataContainer.getUpdatedDateTime());
+		update(handle, "UPDATE public.todo SET done = :done, updateddatetime = :updateddatetime where id in (<idlist>)", params);
 	}
 
 	public List<ITodoModel> selectAllOfCategory(PersistenceHandle handle, String categoryId) {
-		return handle.getHandle().createQuery(
-				"SELECT id, description, done, createddatetime, updateddatetime, categoryid FROM \"todo\" WHERE categoryid = :categoryid")
-				.bind("categoryid", categoryId)
-				.map(new TodoMapper()).list();
+		Map<String, Object> params = new HashMap<String, Object> ();
+		params.put("categoryid", categoryId);
+		return selectList(handle, "SELECT id, description, done, createddatetime, updateddatetime, categoryid FROM todo WHERE categoryid = :categoryid", params, new TodoMapper());
 	}
 
 }
